459cc0a46d0ad0e3fc960b40dd0f305f9673784a
Cameron <cdg341@gmail.com>
2023-10-02 22:33:55 -0500

    Set counter to always run in order to show that it works

--- app.js
+++ app.js
@@ -26,8 +26,13 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2023, 9, 24, 15, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDay();
 
+// let futureDate = new Date(2023, 9, 24, 15, 30, 0);
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 17, 30, 0)
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const mins = futureDate.getMinutes();
